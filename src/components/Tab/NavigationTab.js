bd05f947be12d81d6f7013976ec5ede0a64895f9
Phan Van Nhan <nhanphanvan99@gmail.com>
2021-05-23 17:22:18 +0700

    change profile layout

--- src/components/Tab/NavigationTab.js
+++ src/components/Tab/NavigationTab.js
@@ -1,51 +1,52 @@
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import React from 'react'
-import HomePage from '../../screens/HomePage';
-import Profile from '../../screens/Profile';
-import Cinema from '../../screens/Cinema';
-import TempScreen from '../../screens/TempScreen';
-import AuthScreen from '../../screens/AuthScreen';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from "react";
+import HomePage from "../../screens/HomePage";
+import Profile from "../../screens/Profile";
+import Cinema from "../../screens/Cinema";
+import TempScreen from "../../screens/TempScreen";
+import AuthScreen from "../../screens/AuthScreen";
+
 const NavigationTab = () => {
-    const Tab = createBottomTabNavigator();
-    return (
-      <Tab.Navigator
-        initialRouteName="Movies"
-        tabBarOptions={{
-          activeTintColor: '#e91e63',
+  const Tab = createBottomTabNavigator();
+  return (
+    <Tab.Navigator
+      initialRouteName="Movies"
+      tabBarOptions={{
+        activeTintColor: "#e91e63",
+      }}
+    >
+      <Tab.Screen
+        name="Movies"
+        component={HomePage}
+        options={{
+          tabBarLabel: "Trang Chủ",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
         }}
-      >
-        <Tab.Screen
-          name="Movies"
-          component={HomePage}
-          options={{
-            tabBarLabel: 'Trang Chủ',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="home" color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Notifications"
-          component={Cinema}
-          options={{
-            tabBarLabel: 'Đặt vé',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="bell" color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Profile"
-          component={AuthScreen}
-          options={{
-            tabBarLabel: 'Tài khoản',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="account" color={color} size={size} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    );
-}
-export default NavigationTab
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={Cinema}
+        options={{
+          tabBarLabel: "Đặt vé",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="bell" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: "Tài khoản",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+export default NavigationTab;
